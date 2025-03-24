3d2b4138538675d3bdd31117a0db557db4753689
Bruno Hugo <brunohugo2007@gmail.com>
2025-03-24 17:18:14 -0300

    removing cloud variables

--- backend/config/cloudinaryConn.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary');
-
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY_CLOUD,
-    api_secret: process.env.API_SECRET_CLOUD
-})
-
-module.exports = cloudinary;
